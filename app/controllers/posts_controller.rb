79d466cef4c37ef74b9a3f0cbae512215c931aac
Kazuho1222 <destiny5914@gmail.com>
2023-09-29 07:28:43 +0900

    非同期通信後の処理を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -43,8 +43,8 @@ class PostsController < ApplicationController
 
   def search
     return nil if params[:keyword] == ""
-    tag = Tag.where(['tag_name LIKE ?', "%#{params[:keyword]}"])
-    render json:{keyword: tag}
+    tag = Tag.where(['tag_name LIKE ?', "%#{params[:keyword]}%"] )
+    render json:{ keyword: tag }
   end
 
   private
--- app/javascript/tag.js
+++ app/javascript/tag.js
@@ -5,9 +5,28 @@ document.addEventListener("DOMContentLoaded", () => {
     inputElement.addEventListener("input", () => {
       const keyword = document.getElementById("post_form_tag_name").value;
       const XHR = new XMLHttpRequest();
-      XHR.open("GET", `/post/search/?keyword=${keyword}`, true);
+      XHR.open("GET", `/posts/search/?keyword=${keyword}`, true);
       XHR.responseType = "json";
       XHR.send();
+      XHR.onload = () => {
+        const searchResult = document.getElementById("search-result");
+        searchResult.innerHTML = "";
+        if (XHR.response) {
+          const tagName = XHR.response.keyword;
+          tagName.forEach((tag) => {
+            const childElement = document.createElement("div");
+            childElement.setAttribute("class", "child");
+            childElement.setAttribute("id", tag.id);
+            childElement.innerHTML = tag.tag_name;
+            searchResult.appendChild(childElement);
+            const clickElement = document.getElementById(tag.id);
+            clickElement.addEventListener("click", () => {
+              document.getElementById("post_form_tag_name").value = clickElement.textContent;
+              clickElement.remove();
+            });
+          });
+        };
+      };
     });
   };
 });
